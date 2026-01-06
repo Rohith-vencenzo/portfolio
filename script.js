31461186d3690cef1ea24e80f9941051b97b24f2
Rohith-vencenzo <rohithbhavani20@gmail.com>
2026-01-06 10:38:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function explore() {
+    document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+  }
+  
+  function contactMe() {
+    alert("Email: rohithkumar@email.com");
+  }
+  
